c8ec8fd1db6d7a9527ecc42bd770f0e911a3ec84
HagarHassan11 <hagar.hassan604@gmail.com>
2025-01-17 15:32:40 +0200

    requirments added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'FaceDetection',
+    version= '0.0.0',
+    author= 'Hagar Hassan',
+    author_email= 'hagar.hassan64@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
